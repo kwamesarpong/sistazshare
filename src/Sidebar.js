1708b8b5daffa79b4604c33b34b314f9bdeeef08
kwamesarpong <kayosarp@gmail.com>
2018-08-31 22:24:24 +0000

    Read Story and Sidebar fixed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Image } from 'react-native';
 import { Icon, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
@@ -8,72 +8,70 @@ export default class Sidebar extends React.Component {
     render(){
         return (
             <View>
-                <View style={{ borderBottomWidth: 1, borderBottomColor: '#f4f4f4', height: '90%' }}>
-                    <View style={{ borderBottomWidth: 1, borderBottomColor: '#f4f4f4', paddingTop: 30, paddingBottom: 30, paddingLeft: 30 }}>
-                        <Text style={{ color: '#737373' }}>Kay Sarp</Text>
-                        <Text style={{ color: '#b3b3b3', fontSize: 15, paddingTop: 5 }}>Software Developer</Text>
-                    </View>
-
-                    <TouchableOpacity
-                        onPress={() => Actions.home()}
-                        style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
-                        activeOpacity={0.6}>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-                            <View style={{ width: '25%'}}>
-                                <Icon name='md-sync' style={{color: '#737373', paddingLeft:30}} />
-                            </View>
-                            <View style={{ width: '75%'}}>
-                                <Text style={{ color: '#737373', paddingTop: 5 }}>Requests</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        onPress={() => Actions.facilities()}
-                        style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
-                        activeOpacity={0.6}>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-                            <View style={{ width: '25%'}}>
-                                <Icon name='md-home' style={{color: '#737373', paddingLeft:30}} />
-                            </View>
-                            <View style={{ width: '75%'}}>
-                                <Text style={{ color: '#737373', paddingTop: 5 }}>Facilities</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        onPress={() => Actions.teammember()}
-                        style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
-                        activeOpacity={0.6}>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-                            <View style={{ width: '25%'}}>
-                                <Icon name='md-person' style={{color: '#737373', paddingLeft:30}} />
-                            </View>
-                            <View style={{ width: '75%'}}>
-                                <Text style={{ color: '#737373', paddingTop: 5 }}>Team Members</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        onPress={() => Actions.account()}
-                        style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
-                        activeOpacity={0.6}>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-                            <View style={{ width: '25%'}}>
-                                <Icon name='md-settings' style={{color: '#737373', paddingLeft:30}} />
-                            </View>
-                            <View style={{ width: '75%'}}>
-                                <Text style={{ color: '#737373', paddingTop: 5 }}>Account Settings</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-
+                <View style={{ backgroundColor: '#2DD7D9', borderBottomWidth: 1, borderBottomColor: '#f4f4f4', paddingTop: 30, paddingBottom: 30, paddingLeft: 30 }}>
+                    <Image
+                        style={{ width: 60, height: 60, borderRadius: 50, marginBottom: 10 }}
+                        source={require('../assets/avatar.png')}/>
+                    <Text style={{ color: '#fff' }}>Kay Sarp</Text>
+                    <Text style={{ color: '#fff', fontSize: 15, paddingTop: 5 }}>Software Developer</Text>
                 </View>
-                <TouchableOpacity style={{ alignItems: 'center', backgroundColor: '#0176ff', height: '10%', paddingTop: 20 }} activeOpacity={0.9}>
-                    <Text style={{color: '#fff'}}>Log out</Text>
+
+                <TouchableOpacity
+                    onPress={() => Actions.home()}
+                    style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
+                    activeOpacity={0.6}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <View style={{ width: '25%'}}>
+                            <Icon name='ios-home' style={{color: '#2DD7D9', paddingLeft:30}} />
+                        </View>
+                        <View style={{ width: '75%'}}>
+                            <Text style={{ color: '#2DD7D9', paddingTop: 5 }}>Home</Text>
+                        </View>
+                    </View>
                 </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => Actions.facilities()}
+                    style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
+                    activeOpacity={0.6}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <View style={{ width: '25%'}}>
+                            <Icon name='ios-list-box' style={{color: '#2DD7D9', paddingLeft:30}} />
+                        </View>
+                        <View style={{ width: '75%'}}>
+                            <Text style={{ color: '#2DD7D9', paddingTop: 5 }}>Categories</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => Actions.teammember()}
+                    style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
+                    activeOpacity={0.6}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <View style={{ width: '25%'}}>
+                            <Icon name='ios-star' style={{color: '#2DD7D9', paddingLeft:30}} />
+                        </View>
+                        <View style={{ width: '75%'}}>
+                            <Text style={{ color: '#2DD7D9', paddingTop: 5 }}>Points</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => Actions.account()}
+                    style={{flexDirection:'row', alignItems:'center', paddingTop:20}}
+                    activeOpacity={0.6}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <View style={{ width: '25%'}}>
+                            <Icon name='ios-settings' style={{color: '#2DD7D9', paddingLeft:30}} />
+                        </View>
+                        <View style={{ width: '75%'}}>
+                            <Text style={{ color: '#2DD7D9', paddingTop: 5 }}>Settings</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
             </View>
         )
     }
--- src/StoryCard.js
+++ src/StoryCard.js
@@ -1,86 +1,93 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Image, ActionSheet } from 'react-native';
-import { Icon, Button } from 'native-base';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { Icon, Button, ActionSheet } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 
+var BUTTONS = ["Keep", "Report", "Cancel"];
+var CANCEL_INDEX = 2;
+
+class StoryCard extends Component {
+  state = {
+    indexClicked: ''
+  }
 
 
-const StoryCard = (props) => {
-  return (
-    <TouchableOpacity
-      style={{ backgroundColor: '#fff', borderBottomColor: '#f4f4f4', borderBottomWidth: 1 }}
-      activeOpacity={0.8}
-      onPress={() => console.log("Hi")}>
+  render(){
+    return (
+      <TouchableOpacity
+        style={{ backgroundColor: '#fff', borderBottomColor: '#f4f4f4', borderBottomWidth: 1 }}
+        activeOpacity={0.8}
+        onPress={() => console.log("Hi")}>
 
-      <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 20, marginBottom: 20 }}>
-        <View style={{ paddingTop: 20, width: '25%' }}>
-          <Image
-            style={{ width: 60, height: 60, borderRadius: 50 }}
-            source={require('../assets/avatar.png')}/>
-        </View>
-        <View style={{ paddingTop: 30, width: '60%' }}>
-          <Text style={{ color: '#555555', fontSize: 16 }}>Anna Daniels</Text>
-          <Text style={{ color: '#b3b3b3', fontSize: 13 }}>Health · A few seconds ago</Text>
-        </View>
-        <View style={{ paddingTop: 30, width: '15%' }}>
-          <Button
-            transparent
-            /* onPress={() =>
-              ActionSheet.show(
-                {
-                  options: BUTTONS,
-                  cancelButtonIndex: CANCEL_INDEX,
-                  destructiveButtonIndex: DESTRUCTIVE_INDEX,
-                  title: "Testing ActionSheet"
-                },
-                buttonIndex => {
-                  this.setState({ clicked: BUTTONS[buttonIndex] });
-                }
-              )} */>
-            <Icon
-              name='md-more'
-              style={{ color: '#848484' }} />
-          </Button>
-        </View>
-      </View>
-
-      <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 20, paddingRight: 20, marginBottom: 20 }}>
-        {/* <View style={{ width: '25%' }}></View> */}
-        <View style={{ width: '100%' }}>
-          <Text style={{ color: '#000000', marginBottom: 5 }}>My Secret Fantasies</Text>
-          <Text style={{ color: '#848484' }}>The increment of the thermal heat of a system is equal to the increment of the internal heat of a system...READ STORY</Text>
-        </View>
-      </View>
-      
-      <View style={{ paddingLeft: 20, paddingRight: 20 }}>
-        <View style={{ flex: 1, flexDirection: 'row', marginBottom: 20, borderTopColor: '#f4f4f4', borderTopWidth: 1, paddingTop: 10 }}>
-          <View style={{ alignItems: 'center', width: '30%', flexDirection: 'row' }}>
-            <Icon
-              name='comment'
-              style={{ color: '#848484', fontSize: 20 }}
-              type='EvilIcons' />
-            <Text style={{ color: '#848484', fontSize: 13 }}>3 Snaps</Text>
+        <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 20, marginBottom: 20 }}>
+          <View style={{ paddingTop: 20, width: '25%' }}>
+            <Image
+              style={{ width: 60, height: 60, borderRadius: 50 }}
+              source={require('../assets/avatar.png')}/>
           </View>
-          <View style={{ alignItems: 'center', width: '40%', flexDirection: 'row' }}>
-            <Icon
-              name='comment'
-              style={{ color: '#848484', fontSize: 20 }}
-              type='EvilIcons' />
-            <Text style={{ color: '#848484', fontSize: 13 }}>10 comments</Text>
+          <View style={{ paddingTop: 30, width: '60%' }}>
+            <Text style={{ color: '#555555', fontSize: 16 }}>Anna Daniels</Text>
+            <Text style={{ color: '#b3b3b3', fontSize: 13 }}>Health · A few seconds ago</Text>
           </View>
-          <View style={{ alignItems: 'center', width: '30%', flexDirection: 'row' }}>
-            <Icon
-              name='share-google'
-              style={{ color: '#848484', fontSize: 20 }}
-              type='EvilIcons' />
-            <Text style={{ color: '#848484', fontSize: 13 }}>Share</Text>
+          <View style={{ paddingTop: 30, width: '15%' }}>
+            <Button
+              transparent
+              onPress={() =>
+                ActionSheet.show(
+                  {
+                    options: BUTTONS,
+                    cancelButtonIndex: CANCEL_INDEX,
+                  },
+                  buttonIndex => {
+                    this.setState({ indexClicked: BUTTONS[buttonIndex] });
+                  }
+                )}>
+              <Icon
+                name='md-more'
+                style={{ color: '#848484' }} />
+            </Button>
           </View>
         </View>
-      </View>
 
-    </TouchableOpacity>
-  );
+        <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 20, paddingRight: 20, marginBottom: 20 }}>
+          {/* <View style={{ width: '25%' }}></View> */}
+          <View style={{ width: '100%' }}>
+            <Text style={{ color: '#000000', marginBottom: 5 }}>My Secret Fantasies</Text>
+            <Text style={{ color: '#848484' }}>The increment of the thermal heat of a system is equal to the increment of the internal heat of a system...READ STORY</Text>
+          </View>
+        </View>
+        
+        <View style={{ paddingLeft: 20, paddingRight: 20 }}>
+          <View style={{ flex: 1, flexDirection: 'row', marginBottom: 20, borderTopColor: '#f4f4f4', borderTopWidth: 1, paddingTop: 10 }}>
+            <View style={{ alignItems: 'center', width: '30%', flexDirection: 'row' }}>
+              <Icon
+                name='comment'
+                style={{ color: '#848484', fontSize: 20 }}
+                type='EvilIcons' />
+              <Text style={{ color: '#848484', fontSize: 13 }}>3 Snaps</Text>
+            </View>
+            <View style={{ alignItems: 'center', width: '40%', flexDirection: 'row' }}>
+              <Icon
+                name='comment'
+                style={{ color: '#848484', fontSize: 20 }}
+                type='EvilIcons' />
+              <Text style={{ color: '#848484', fontSize: 13 }}>10 comments</Text>
+            </View>
+            <View style={{ alignItems: 'center', width: '30%', flexDirection: 'row' }}>
+              <Icon
+                name='share-google'
+                style={{ color: '#848484', fontSize: 20 }}
+                type='EvilIcons' />
+              <Text style={{ color: '#848484', fontSize: 13 }}>Share</Text>
+            </View>
+          </View>
+        </View>
+
+      </TouchableOpacity>
+    );
+  }
+
 }
 
 export default StoryCard
